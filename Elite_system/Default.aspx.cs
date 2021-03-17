aeba1bf0737c2198a43be9e218d0016badce968a
rami.bmw33@yahoo.com <rami.bmw33@yahoo.com>
2021-03-17 15:41:10 +0200

    a

--- Elite_system/Default.aspx.cs
+++ Elite_system/Default.aspx.cs
@@ -34,25 +34,17 @@ namespace Elite_system
             cmd.CommandType = CommandType.Text;
             cmd.CommandText = "Select id , FreezTo from [dbo].[Medical_Types_And_Companies] where Freez = 1";
             SqlDataReader reader4 = cmd.ExecuteReader();
+            List<string> ID = new List<string>();
+            List<string> FreezTo = new List<string>();
             try
             {
-               
+                
                 while (reader4.Read())
                 {
-                   
                     if (!reader4.IsDBNull(0))
                     {
-                        var x = reader4[1].ToString().Substring(0, 10);
-                        var y = DateTime.UtcNow.AddHours(2).ToString().Substring(0,10);
-                        if ( x == y)
-                        {
-                            cmd.Parameters.Clear();
-                            cmd.Connection = con;
-                       
-                            cmd.CommandText = "update [dbo].[Medical_Types_And_Companies] set FreezTo = Null , FreezFrom = Null , Freez = 0 where id = '" + reader4[0].ToString() + "'  ";
-                            reader4.Close();
-                            cmd.ExecuteNonQuery();
-                        }
+                        ID.Add(reader4[0].ToString());
+                        FreezTo.Add(reader4[1].ToString().Substring(0, 10));
                     }
                 }
             }
@@ -67,6 +59,34 @@ namespace Elite_system
             {
                 reader4.Close();
                 Cls_Connection.close_connection();
+            }
+
+
+
+            try
+            {
+                for (int i = 0; i < ID.Count; i++)
+                {
+                    var _ID = ID[i].ToString();
+                    var _FreezTo = FreezTo[i].ToString();
+                    
+                    var x = _FreezTo.ToString().Substring(0, 10);
+                    var y = DateTime.UtcNow.AddHours(2).ToString().Substring(0, 10);
+                    if (x == y)
+                    {
+                        cmd.Parameters.Clear();
+                        cmd.Connection = con;
+                        cmd.CommandText = "update [dbo].[Medical_Types_And_Companies] set FreezTo = Null , FreezFrom = Null , Freez = 0 where id = '" + _ID+ "'  ";
+                        Cls_Connection.open_connection();
+                        cmd.ExecuteNonQuery();
+                        Cls_Connection.close_connection();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Cls_Connection.close_connection();
+
             }
 
         }
